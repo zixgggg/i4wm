c2fdca32e5558b7f355e4d1e7e3f9890703397b1
zixgggg <zixzix035@gmail.com>
2026-01-11 18:54:24 +0800

    call rofi function

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import subprocess
 from Xlib.display import Display
 from Xlib import X, XK
 dpy = Display()
+screen = dpy.screen()
+root = screen.root
+root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("m")),X.Mod4Mask,1,X.GrabModeAsync,X.GrabModeAsync)
+def call_rofi():
+    if ev.type==X.KeyPress:
+        #print(f"press key:{ev.detail}")
+        if ev.detail==dpy.keysym_to_keycode(XK.string_to_keysym("m")):
+            subprocess.Popen(["rofi","-show","drun"])
+while True:
+    ev=dpy.next_event()
+    call_rofi()
+    dpy.flush()
